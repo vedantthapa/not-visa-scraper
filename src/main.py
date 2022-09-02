4505e4778950d4eea5e8a55668e6553641966389
vedantthapa <thapavedant@gmail.com>
2022-09-02 00:10:47 -0300

    feat: add utils and main code

---
+++ src/main.py
@@ -0,0 +1,82 @@
+import json
+import time
+from pprint import pprint
+from random import uniform
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+from utils import log_in, send_message
+
+url = "https://ais.usvisa-info.com/en-ca/niv/schedule/43160383/appointment"
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+# logging in
+log_in(driver)
+
+action = ActionChains(driver)
+
+#
+availability = {}
+
+location_select = Select(
+    driver.find_element(By.ID, "appointments_consulate_appointment_facility_id")
+)
+location_options = location_select.options[1:]
+for option in location_options:
+    option.click()
+    availability[option.text] = {}
+    time.sleep(uniform(2, 4))
+
+    datepicker = driver.find_element(By.ID, "appointments_consulate_appointment_date")
+    try:
+        datepicker.click()
+    except ElementNotInteractableException:
+        print(f"No availability in city: {option.text}")
+        continue
+    time.sleep(uniform(2, 4))
+
+    a_elements_list = []
+    next_click_counter = 15
+
+    while not a_elements_list and next_click_counter != 0:
+        next_btn = driver.find_element(
+            By.XPATH, '//*[@id="ui-datepicker-div"]/div[2]/div/a'
+        )
+        next_btn.click()
+        time.sleep(uniform(0.5, 1.5))
+
+        a_elements = driver.find_elements(
+            By.XPATH, "//table[@class='ui-datepicker-calendar']//a"
+        )
+        a_elements_list.extend(a_elements)
+
+        next_click_counter -= 1
+
+    if a_elements_list:
+        a_elements_list[0].click()
+    else:
+        action.move_to_element(
+            driver.find_element(
+                By.XPATH, '//*[@id="appointment-form"]/fieldset/ol/fieldset/legend'
+            )
+        )
+        action.click()
+        action.perform()
+
+        continue
+
+    date_val = datepicker.get_attribute("value")
+
+    time_select = Select(
+        driver.find_element(By.ID, "appointments_consulate_appointment_time")
+    )
+    availability[option.text][date_val] = time_select.options[1:].text
+
+send_message(json.dumps(availability))
---
+++ src/utils.py
@@ -0,0 +1,46 @@
+# from pyvirtualdisplay import Display
+import os
+import time
+from random import uniform
+
+import requests
+from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
+
+load_dotenv()
+
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{os.environ.get('TOKEN')}/sendMessage"
+    parameters = {"chat_id": os.environ.get("CHAT_ID"), "text": text}
+    return requests.post(url, parameters)
+
+
+def log_in(driver):
+    # Clicking the first prompt, if there is one
+    try:
+        driver.find_element(By.XPATH, "/html/body/div[6]/div[3]/div/button").click()
+        time.sleep(uniform(2, 4))
+    except:
+        pass
+
+    # Filling the user and password
+    user_box = driver.find_element(By.NAME, "user[email]")
+    user_box.send_keys(os.environ.get("USERNAME"))
+    time.sleep(uniform(2, 4))
+
+    password_box = driver.find_element(By.NAME, "user[password]")
+    password_box.send_keys(os.environ.get("PASSWORD"))
+    time.sleep(uniform(2, 4))
+
+    # Clicking the checkbox
+    driver.find_element(By.XPATH, '//*[@id="new_user"]/div[3]/label/div').click()
+    time.sleep(uniform(1, 2.5))
+
+    # Clicking 'Sign in'
+    driver.find_element(By.XPATH, '//*[@id="new_user"]/p[1]/input').click()
+    time.sleep(uniform(1, 2.5))
+
+    # Waiting for the page to load.
+    # 5 seconds may be ok for a computer, but it doesn't seem enougn for the Raspberry Pi 4.
+    time.sleep(6)
