083c93e8ee7c736e9d636b9828c3848687e56789
vedantthapa <thapavedant@gmail.com>
2023-05-14 19:56:14 -0300

    feat(main): update html tags

--- src/main.py
+++ src/main.py
@@ -1,22 +1,39 @@
+import datetime
 import json
+import logging
+import os
 import time
 from random import uniform
 
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.common.exceptions import ElementNotInteractableException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.options import Options
 
 from utils import log_in, send_message
 
-url = "https://ais.usvisa-info.com/en-ca/niv/schedule/43160383/appointment"
+load_dotenv()
 
-driver = webdriver.Chrome()
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+
+script_exec_dt = datetime.datetime.now()
+
+url = f"https://ais.usvisa-info.com/en-ca/niv/schedule/{os.environ.get('URL_ID')}/appointment"
+
+# Setting Chrome options to run the scraper headless.
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get(url)
+logging.info("Driver connected.")
 
 # logging in
 log_in(driver)
+logging.info("User logged in successfully.")
 
 action = ActionChains(driver)
 
@@ -28,6 +45,9 @@ location_select = Select(
     driver.find_element(By.ID, "appointments_consulate_appointment_facility_id")
 )
 location_options = location_select.options[1:]
+
+logging.info("Attempting to browse through the Location dropdown...")
+
 for option in location_options:
     option.click()
     availability[option.text] = {}
@@ -38,42 +58,44 @@ for option in location_options:
     try:
         datepicker.click()
     except ElementNotInteractableException:
-        print(f"No availability in city: {option.text}")
+        logging.info(f"Datepicker not available in city: {option.text}")
         continue
     time.sleep(uniform(2, 4))
 
     # initialize a list to store the clickable dates (<a> tags)
-    # initialize a counter to set an upper bound on clicking next
-    a_elements_list = []
-    next_click_counter = 15
+    date_elements_list = []
 
-    while not a_elements_list and next_click_counter != 0:
-        next_btn = driver.find_element(
-            By.XPATH, '//*[@id="ui-datepicker-div"]/div[2]/div/a'
-        )
-        next_btn.click()
-        time.sleep(uniform(0.5, 1.5))
+    # how many months to look at
+    next_click_counter = 4
 
-        a_elements = driver.find_elements(
+    while not date_elements_list and next_click_counter != 0:
+        date_elements = driver.find_elements(
             By.XPATH, "//table[@class='ui-datepicker-calendar']//a"
         )
-        a_elements_list.extend(a_elements)
+        date_elements_list.extend(date_elements)
+
+        driver.find_element(
+            By.XPATH, '//*[@id="ui-datepicker-div"]/div[2]/div/a'
+        ).click()
+        time.sleep(uniform(0.5, 1.5))
 
         next_click_counter -= 1
 
-    if a_elements_list:
+    if date_elements_list:
         # select the first date
-        a_elements_list[0].click()
+        date_elements_list[0].click()
+        logging.info(f"Found availability at: {option.text}")
     else:
         # click on a trivial element to close the datepicker
         action.move_to_element(
             driver.find_element(
-                By.XPATH, '//*[@id="appointment-form"]/fieldset/ol/fieldset/legend'
+                By.XPATH,
+                '//*[@id="appointment-form"]/fieldset/ol/fieldset/div/div[3]/div[1]/strong',
             )
         )
-        action.click()
-        action.perform()
+        action.click().perform()
 
+        logging.info(f"Browsed through city: {option.text}. No availability found")
         continue
 
     date_val = datepicker.get_attribute("value")
@@ -84,5 +106,11 @@ for option in location_options:
     )
     availability[option.text][date_val] = time_select.options[1:].text
 
+message = f"""
+Script executed at {script_exec_dt}.
+
+Availability JSON: {json.dumps(obj=availability, indent=4, sort_keys=True)}
+"""
+
 # send the json to telegram
-send_message(json.dumps(availability))
+send_message(message)
