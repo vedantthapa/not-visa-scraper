ffbdc6df66652c229691e94f6461f06e35c89452
vedantthapa <thapavedant@gmail.com>
2023-07-05 13:51:41 -0300

    feat(cron_script): add conda activate instructions

---
+++ cron_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Change to the desired directory
+cd ~/Downloads/not-visa-scraper/
+
+# Activate the conda environment
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate not-visa-scraper
+
+# Run the Python script
+python src/main.py
